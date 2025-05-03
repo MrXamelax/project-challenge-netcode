7cfd8b1377c13415d77a539375754720cdf1a70d
Theo Stötzer <hallo>
2025-05-04 00:15:33 +0200

    Logging refinements
    - Improved efficiency of logging itself

--- Assets/LetsFetz/Scripts/Manger/LoggingManager.cs
+++ Assets/LetsFetz/Scripts/Manger/LoggingManager.cs
@@ -12,10 +12,14 @@ public class LoggingManager : MonoBehaviour {
     
     private List<string> _loggingData = new List<string>();
 
+    private Dictionary<ulong, TeamManager> _teamManagers;
+    private Dictionary<ulong, NewNetworkFirstPersonController> _nnfpControllers;
+
     private float _timeStart;
     private string _loggingFolderPath;
 
     public enum LoggingType {
+        Regular,
         DamageDone, DamageTaken, StopRegeneration,
         Respawn, Death, 
         CaptureDataStation, CaptureYellowZeal, CaptureRedZeal, CaptureGasLeak,
@@ -33,8 +37,18 @@ public class LoggingManager : MonoBehaviour {
 
     }
 
+    private void Initialize() {
+        _teamManagers = new Dictionary<ulong, TeamManager>();
+        _nnfpControllers = new Dictionary<ulong, NewNetworkFirstPersonController>();
+        foreach (var client in NetworkManager.Singleton.ConnectedClientsList) {
+            _teamManagers.Add(client.ClientId, client.PlayerObject.GetComponent<TeamManager>());
+            _nnfpControllers.Add(client.ClientId, client.PlayerObject.GetComponent<NewNetworkFirstPersonController>());
+        }
+    }
+
     public void ToggleLogging(bool start) {
         if (start) {
+            Initialize();
             Debug.Log("Start Logging");
             _timeStart = Time.time;
             _cLoggingCycle = StartCoroutine(LoggingCycle());
@@ -52,17 +66,54 @@ public class LoggingManager : MonoBehaviour {
         }
     }
 
-    private void LogRegular() {
+    private string TimeStamp() {
         var timePassed = Time.time - _timeStart;
         var timeSpan = TimeSpan.FromSeconds(timePassed);
         var timeString = timeSpan.ToString(@"mm\:ss\:ff");
-        string data = timeString + ",";
+        return timeString;
+    }
+
+    private void LogRegular() {
+        string data = "";
+        foreach (var client in NetworkManager.Singleton.ConnectedClientsList) {
+            /*
+            var player = client.PlayerObject;
+            var pos = player.transform.position;
+            data += $"{TimeStamp()};";
+            data += $"{MatchManager.Instance.GetPlayerNameByClientID(client.ClientId)};";
+            data += $"{client.ClientId};";
+            data += $"{_teamManagers[client.ClientId].GetLocalTeamID()};";
+            data += $"{pos.x};";
+            data += $"{pos.y};";
+            data += $"{pos.z};";
+            data += $"{_nnfpControllers[client.ClientId].GetHealth()};";
+            data += $"{MatchManager.Instance.GetPointsFromTeam(_teamManagers[client.ClientId].GetLocalTeamID())};";
+            data += "Regular\n";
+            */
+            data += DataEntry(client, LoggingType.Regular);
+        }
         WriteLog(data);
     }
 
-    public void LogEvent() {
+    public void LogEvent(NetworkClient client, LoggingType type) {
+        WriteLog(DataEntry(client, type));
+    }
+    
+    private string DataEntry(NetworkClient client, LoggingType type) {
         string data = "";
-        WriteLog(data);
+        var player = client.PlayerObject;
+        var pos = player.transform.position;
+        data += $"{TimeStamp()};";
+        data += $"{MatchManager.Instance.GetPlayerNameByClientID(client.ClientId)};";
+        data += $"{client.ClientId};";
+        data += $"{_teamManagers[client.ClientId].GetLocalTeamID()};";
+        data += $"{pos.x};";
+        data += $"{pos.y};";
+        data += $"{pos.z};";
+        data += $"{_nnfpControllers[client.ClientId].GetHealth()};";
+        data += $"{MatchManager.Instance.GetPointsFromTeam(_teamManagers[client.ClientId].GetLocalTeamID())};";
+        data += $"{type.ToString()}\n";
+        return data;
     }
 
     private void WriteLog(string message) {
@@ -70,16 +121,17 @@ public class LoggingManager : MonoBehaviour {
     }
 
     private string PrepareLog() {
-        string data = "Timestamp,Playername,PlayerID,TeamID,x,y,z,HP,Event\n";
+        //TODO: Add points (of every contract?) to log
+        string data = "Timestamp;Playername;PlayerID;TeamID;x;y;z;HP;Points;Event\n";
         foreach (var line in _loggingData) {
-            data += line + ";\n";
+            data += line;
         }
         return data;
     }
 
     private void ExportData() {
-        //TODO: unique file name
-        string filename = $"{DateTime.Now:yyyy-MM-dd_HH-mm-ss}_log.txt";
+        var playername = MatchManager.Instance.GetPlayerNameByClientID(NetworkManager.Singleton.LocalClientId);
+        string filename = $"{DateTime.Now:yyyy-MM-dd_HH-mm-ss}_Log_{playername}.txt";
         _loggingFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/TheoEval/";
         string fullPath = _loggingFolderPath + filename;
         //string externalPath = Application.persistentDataPath + "/" + filename;
